f7cc918e5e8a8c8f8d382243cdc28ebaf49c541d
dakshilgorasiya <dakshilgorasiya2005@gmail.com>
2024-03-27 17:29:41 +0530

    user model and video model added

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,9 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect();
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
     console.log(
       "\n MongoDB connected !! DB Host: ",
       connectionInstance.connection.host
