ecab8814cbc09816d7c1f90e5d1b15ecafe5a428
dakshilgorasiya <dakshilgorasiya2005@gmail.com>
2024-03-28 22:01:31 +0530

    first rougt added

--- src/app.js
+++ src/app.js
@@ -28,4 +28,10 @@ app.use(express.static("public"));
 
 app.use(cookieParser());
 
+//routes import
+import userRoutes from "./routes/user.routes.js";
+
+//routes delcaration
+app.use("/api/v1/users", userRoutes);
+
 export { app };
