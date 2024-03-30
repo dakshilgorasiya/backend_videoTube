60d61660a31c44ec4d15476fbb34257d953f4b05
dakshilgorasiya <dakshilgorasiya2005@gmail.com>
2024-03-30 17:12:13 +0530

    Refactor video controller and add get video by id functionlity

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,47 +1,104 @@
-import mongoose, {isValidObjectId} from "mongoose"
-import {Video} from "../models/video.model.js"
-import {User} from "../models/user.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
-import {uploadOnCloudinary} from "../utils/cloudinary.js"
-
+import mongoose, { isValidObjectId } from "mongoose";
+import { Video } from "../models/video.model.js";
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 const getAllVideos = asyncHandler(async (req, res) => {
-    const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
-    //TODO: get all videos based on query, sort, pagination
-})
+  const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
+  //TODO: get all videos based on query, sort, pagination
+});
 
 const publishAVideo = asyncHandler(async (req, res) => {
-    const { title, description} = req.body
-    // TODO: get video, upload to cloudinary, create video
-})
+  // Get the video file and thumbnail from the request
+  // Get the title and description from the request body
+  // Get duration from cloudanary
+  // Create a new video object
+  // Save the video to the database
+  // Return the video object in the response
+  const { title, description } = req.body;
+  // TODO: get video, upload to cloudinary, create video
+
+  if ([title, description].some((filed) => filed?.trim() === "")) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const videoLocalPath = req.files?.videoFile[0]?.path;
+  if (!videoLocalPath) {
+    throw new ApiError(400, "Video file is required");
+  }
+
+  const thumbnailLocalPath = req.files?.thumbnail[0]?.path;
+  if (!thumbnailLocalPath) {
+    throw new ApiError(400, "Thumbnail is required");
+  }
+
+  const video = await uploadOnCloudinary(videoLocalPath);
+  if (!video) {
+    throw new ApiError(500, "Failed to upload video");
+  }
+
+  const thumbnail = await uploadOnCloudinary(thumbnailLocalPath);
+  if (!thumbnail) {
+    throw new ApiError(500, "Failed to upload thumbnail");
+  }
+
+  const duration = video?.duration;
+
+  const uploadedVideo = await Video.create({
+    title,
+    description,
+    videoFile: video?.url,
+    thumbnail: thumbnail?.url,
+    duration,
+    owner: req.user._id,
+  });
+
+  res
+    .status(201)
+    .json(new ApiResponse(201, uploadedVideo, "Video uploaded successfully"));
+});
 
 const getVideoById = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
-    //TODO: get video by id
-})
+  // Get the video id from the request params
+  // Find the video by id
+  // Return the video object in the response
+  //TODO: get video by id
+  const { videoId } = req.params;
+  if (!videoId?.trim()) {
+    throw new ApiError(400, "Video id is required");
+  }
+
+  const video = await Video.findById(videoId);
+
+  if (!video) {
+    throw new ApiError(404, "Video not found");
+  }
+
+  res.status(200).json(new ApiResponse(200, video, "Video found"));
+});
 
 const updateVideo = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
-    //TODO: update video details like title, description, thumbnail
-
-})
+  const { videoId } = req.params;
+  //TODO: update video details like title, description, thumbnail
+});
 
 const deleteVideo = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
-    //TODO: delete video
-})
+  const { videoId } = req.params;
+  //TODO: delete video
+});
 
 const togglePublishStatus = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
-})
+  const { videoId } = req.params;
+});
 
 export {
-    getAllVideos,
-    publishAVideo,
-    getVideoById,
-    updateVideo,
-    deleteVideo,
-    togglePublishStatus
-}
+  getAllVideos,
+  publishAVideo,
+  getVideoById,
+  updateVideo,
+  deleteVideo,
+  togglePublishStatus,
+};
