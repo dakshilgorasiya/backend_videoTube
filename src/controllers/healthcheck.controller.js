73d08aba0835f81a5d8d1e95797353cbde3f4ad5
dakshilgorasiya <dakshilgorasiya2005@gmail.com>
2024-03-30 17:43:40 +0530

    Add healthcheck response with OK status and message

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -3,7 +3,14 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const healthCheck = asyncHandler(async (req, res) => {
-    // TODO: build a healthcheck response that simply returns the OK status as json with a message
+  // TODO: build a healthcheck response that simply returns the OK status as json with a message
+  res.send(
+    new ApiResponse(
+      200,
+      { message: "OK" },
+      "Connected to the server successfully!"
+    )
+  );
 });
 
 export { healthCheck };
